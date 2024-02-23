d33f7606628b70591449950df3fd892f5aae43da
WenDEVLIFE <frouenmedinajr@gmail.com>
2024-02-23 13:12:35 +0800

    Fixed firebase

--- app/src/main/java/com/example/mherlsmanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/mherlsmanagementsystem/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.mherlsmanagementsystem;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        button = findViewById(R.id.loginbutton);
+        button.setOnClickListener(v -> {
+            // Do something in response to button click
+
+            // Write a message to the database
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference myRef = database.getReference("message");
+
+            myRef.setValue("Hello, World!");
+
+            AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+            alertDialog.setTitle("Alert");
+            alertDialog.setMessage("Welcome to MHERLS Management System");
+            alertDialog.show();
+
+        });
+
     }
 }
