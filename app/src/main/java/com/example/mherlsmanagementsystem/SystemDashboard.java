02d045487c22c28b01d596921c9dbdb097612229
WenDEVLIFE <frouenmedinajr@gmail.com>
2024-02-24 09:36:30 +0800

    💻 added dependency for gif and gif

--- app/src/main/java/com/example/mherlsmanagementsystem/SystemDashboard.java
+++ app/src/main/java/com/example/mherlsmanagementsystem/SystemDashboard.java
@@ -7,8 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.navigation.NavigationView;
 
 public class SystemDashboard extends AppCompatActivity {
@@ -18,6 +20,9 @@ public class SystemDashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_system_dashboard);
+
+        ImageView imageView = findViewById(R.id.sunrisegif);
+        Glide.with(this).load(R.drawable.fieldunscreen).into(imageView);
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         usernametext = navigationView.getHeaderView(0).findViewById(R.id.username);
